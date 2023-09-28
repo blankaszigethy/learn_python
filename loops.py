a0cfe8d6cdbacb6aa3a051e7d5a48dd0c0d71896
Blanka Szigethy <blankaszigethy@gmail.com>
2023-09-28 18:59:04 +0200

    while loop

---
+++ loops.py
@@ -0,0 +1,20 @@
+counter = 0
+sheeps = 3
+
+while(counter != sheeps):
+    count_for_print = counter+1
+
+    print(f"counter={counter} sheeps={sheeps} count_for_print={count_for_print}")
+
+    if count_for_print==1:
+        print(f"{count_for_print} sheep")
+    else:
+        print(f"{count_for_print} sheeps")
+
+    counter = counter+1
+
+
+
+
+print("i am sleeping")
+      
