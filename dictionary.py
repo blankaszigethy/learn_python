e8bbb346c337ec375d84868eb0e8eb3d8dcd55b5
Zsolt Tasnadi <rastasi@gmail.com>
2023-10-06 19:32:09 +0200

    add dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,38 @@
+person = {
+  "first_name": "Blanka",
+  "last_name": "Szigethy",
+  "age": 20,
+  "programming_skill": True
+}
+
+person2 = {
+  "first_name": "Dóra",
+  "last_name": "Pekk-Juhász",
+  "age": 28,
+  "programming_skill": False
+}
+
+person3 = {
+  "first_name": "Zsolt",
+  "last_name": "Tasnádi",
+  "age": 36,
+  "programming_skill": True
+  }
+
+first_name = person["first_name"]
+last_name = person["last_name"]
+age = person["age"]
+
+print(f"{first_name} {last_name}'s age is {age}")
+
+first_name = person2["first_name"]
+last_name = person2["last_name"]
+age = person2["age"]
+
+print(f"{first_name} {last_name}'s age is {age}")
+
+first_name = person3["first_name"]
+last_name = person3["last_name"]
+age = person3["age"]
+
+print(f"{first_name} {last_name}'s age is {age}")
