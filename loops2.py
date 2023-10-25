a8d942da722f77f38745dbf547dd165519f630e7
Blanka Szigethy <blankaszigethy@gmail.com>
2023-10-25 19:53:07 +0200

    add conditions2.py and loops2.py

---
+++ loops2.py
@@ -0,0 +1,20 @@
+num_of_heets = 20
+i = 0
+
+while num_of_heets >= i:
+    print(i)
+    i = i + 1
+
+
+team = [
+    "Manci",
+    "Bubu",
+    "Bellanyi",
+    "Tasi",
+]
+
+j = 0
+
+while j < len(team):
+    print(team[j])
+    j = j + 1
