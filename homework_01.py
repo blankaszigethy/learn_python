eeacdfae78bd817dce24dc366a715b5c0d0ee286
Blanka Szigethy <blankaszigethy@gmail.com>
2023-10-30 21:03:22 +0100

    add homework_01.py

---
+++ homework_01.py
@@ -0,0 +1,54 @@
+SHOTS = [
+    "Tatratea",
+    "Tubi",
+    "Tequila",
+    "Jagermeister",
+    "Abszint",
+]
+
+i = 0 
+
+while i < len(SHOTS):
+    print(SHOTS[i])
+    i = i + 1
+
+PEOPLE = [
+{
+    "first_name": "Dora",
+    "last_name": "Pekk-Juhasz",
+    "favourite": "Tubi",
+},
+{
+    "first_name": "Zsolt",
+    "last_name": "Tasnadi",
+    "favourite": "Tequila",
+},
+{
+    "first_name": "Balazs",
+    "last_name": "Bellanyi",
+    "favourite": "Jagermeister",
+},
+{
+    "first_name": "Mira",
+    "last_name": "Harmati",
+    "favourite": "Tatratea",
+},
+{
+    "first_name": "Blanka",
+    "last_name": "Szigethy",
+    "favourite": "Abszint",
+}
+]
+
+j = 0
+
+while j <len(PEOPLE):
+    first_name = PEOPLE[j]["first_name"]
+    last_name = PEOPLE[j]["last_name"]
+    print(f"{first_name} {last_name}")
+    j = j + 1
+
+    l = 0
+    while
+
+
