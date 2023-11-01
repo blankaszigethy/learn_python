f8a5eb914583af9853f7220faed64ed290fb0af8
Zsolt Tasnadi <rastasi@gmail.com>
2023-11-01 19:08:10 +0100

    fix components homework

--- cocktail/components.py
+++ cocktail/components.py
@@ -5,62 +5,62 @@ COMPONENTS = [
 ]
 
 WINES = [
-    "name": "kéknyelű",
-    "name": "kadarka",
-    "name": "kékfrankos",
-    "name": "furmint",
-    "name": "juhfark"
+    "kéknyelű",
+    "kadarka",
+    "kékfrankos",
+    "furmint",
+    "juhfark"
+]
+
+GINS = [
+    "gordon's",
+    "hendrick's",
+    "beefeater",
+    "bobby's"
+]
+
+SOFT_DRINKS = [
+    "coca cola",
+    "fanta",
+    "sprite",
+    "nestea",
 ]
 
 def print_wines():
     i = 0
     while (i < len(WINES)):
-        wine = WINES[i]
-        name = wine["name"]
-        print(f"{wine['name']}")
-
-
-GINS = [
-    "name": "gordon's",
-    "name": "hendrick's",
-    "name": "beefeater",
-    "name": "bobby's"
-]
+        print(WINES[i])
+        i = i + 1
 
 def print_gins():
-    i = 0 
-    while (i < o len(GINS)):
-        gins = GINS[i]
-        name = gins["name"]
-        print (f"{gins}")
-
-
-SOFT_DRINKS = [
-    "name": "coca cola"
-    "name": "fanta"
-    "name": "sprite"
-    "name": "nestea"
-]
+    i = 0
+    while (i < len(GINS)):
+        print (GINS[i])
+        i = i + 1
 
 def print_soft_drinks():
     i = 0
-    while (i < 0 len(SOFT_DRINKS)):
-        soft_drinks = SOFT_DRINKS[i]
-        name = soft_drinks["name"]
-        print(f"{soft_drinks}")
+    while (i < len(SOFT_DRINKS)):
+        print(SOFT_DRINKS[i])
+        i = i + 1
 
-def print_subcomponents(type):
-    if wine(WINES):
-        print(print_wines)
+def print_subcomponents(category):
+    if category == "wines":
+        print_wines()
 
-    if gin(GINS):
-        print(print_gins)
+    if category == "gins":
+        print_gins()
 
-    if soft_drink(SOFT_DRINKS):
-        print(print_soft_drinks)
+    if category == "soft_drinks":
+        print_soft_drinks()
 
 
 def print_all():
     i = 0
-    while(i < len(COMPONENTS)): 
+    while(i < len(COMPONENTS)):
+        category = COMPONENTS[i]
+        print(f"*** {category} ***")
+        print_subcomponents(category)
+        i = i + 1
 
+print_all()
